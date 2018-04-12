964161c9a9518ad38c5c0a9d98cd727427d0fbd6
Joseph Gagliardo <joey@me.com>
2018-04-12 17:35:40 -0500

    After JPM Houston 2

--- examples/spark/submit-spark-text.sh
+++ examples/spark/submit-spark-text.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 hadoop fs -put /examples/text/shakespeare.txt /
-hadoop fs -rm -r /grep_spark1
+hadoop fs -rm -r /spark-text
 spark-submit spark-text.py
 hadoop fs -ls /spark-text
 hadoop fs -cat /spark-text/*
